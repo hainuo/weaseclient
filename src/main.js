bcd75b2e50db157d1d982658cc6664cae136f041
hainuo <admin@hainuo.inf>
2016-09-16 13:01:07 +0800

    增加router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue';
 import App from './App';
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+
 
 Vue.use(VueResource);
-
+Vue.use(VueRouter);
 /* eslint-disable no-new */
 // const app =
-new Vue({
-  el: 'body',
-  components: { App },
-});
+// new Vue({
+//   el: 'body',
+//   components: { App },
+// });
 // console.log(app);
+
+const router = new VueRouter();
+router.start(App, 'body');
