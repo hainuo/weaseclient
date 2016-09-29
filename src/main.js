0e786a542f6d2913a6cbe65156930dcaecdaf2ae
hainuo <admin@hainuo.info>
2016-09-30 00:40:46 +0800

    up to vue2.0 and add vuerouter vueresource  vue-auth

--- src/main.js
+++ src/main.js
@@ -1,29 +1,43 @@
 import Vue from 'vue';
-import Login from './components/Login';
-
 import VueRouter from 'vue-router';
+import VueAuth from '@websanova/vue-auth';
+import VueResource from 'vue-resource';
+import Login from './components/Login.vue';
+
 Vue.use(VueRouter);
-const Foo = { components: '<template>f00</template>' };
+Vue.use(VueResource);
+// 0. If using a module system, call Vue.use(VueRouter)
+
+// 1. Define route components.
+// These can be imported from other files
+const Bar = { template: '<div>bar</div>' };
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// Vue.extend(), or just a component options object.
+// We'll talk about nested routes later.
 const routes = [
-  { path: '/', componets: Foo },
-  { path: '/login', components: Login },
+  { path: '/login', component: Login },
+  { path: '/bar', component: Bar },
 ];
 
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
 const router = new VueRouter({
-  routes,
+  routes, // short for routes: routes
 });
 
-/* eslint-disable no-new */
-// const app =
-new Vue({
+Vue.use(VueAuth, {
   router,
-  components: { Login },
+});
+// 4. Create and mount the root instance.
+// Make sure to inject the router with the router option to make the
+// whole app router-aware.
+const app = new Vue({
+  router,
 }).$mount('#app');
 
-// new Vue({
-//   el: 'body',
-//   components: { Login },
-//   ready() {
-//     console.log('this is the v-dom ready', this);
-//   },
-// });
+console.log(app);
+// Now the app has started!
